707447f321fbc54c82f0e40bfedfb4a7870598dd
mega9782 <adriangamer955@Gmail.com>
2023-11-15 22:36:43 +0700

    bismillah

--- 04 - Latihan/src/App.java
+++ 04 - Latihan/src/App.java
@@ -1,12 +1,5 @@
-import java.util.Scanner;
 public class App {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int x=8; int y = s.nextInt();
-        if(y>x){
-            System.out.println(y+x*2);
-        }else{
-            System.out.println(y*y/x);
-        }
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
     }
 }
